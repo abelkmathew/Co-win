6618681b4b737fb4af42f9bef6b90e1e23094183
abelkmathew <abelkmathew@gmail.com>
2021-05-28 17:20:14 +0530

    Completed-error fix

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import axios from 'axios'
-import {useState} from 'react'
 
 function Statistics() {
-const [state,setState] = useState([])
+
 
     return (
         <div>
@@ -11,22 +9,7 @@ const [state,setState] = useState([])
                 <div className="container">
                     <label htmlFor=""><h5>Search By Pin</h5></label><br /><br />
                     <input type="text" class="form-control" placeholder="Enter Pincode" maxlength="6" size="6"/><br />
-                    <button onClick={()=>{
-                        axios.get('https://cdn-api.co-vin.in/api/v2/admin/location/states').then((response)=>{
-                            console.log(response.data)
-                            setState(response.data)
-                        })
-                    }} type="submit" className="btn btn-warning ">Submit</button>
-                    {
-                        state.map((obj,index)=>{
-                            return(
-                                <div>
-                                    <h1>{index}</h1>
-                                    <h4>{obj.state}</h4>
-                                </div>
-                            )
-                        })
-                    }
+                    <button type="submit" className="btn btn-warning ">Submit</button>
         
                     <br /><br />
                     <h5>Search by State</h5>
